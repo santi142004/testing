0968530dd990d5800f9d80dccdd12cdaaa219d36
santi142004 <santiagobarreralopez2004@gmail.com>
2023-07-31 08:35:35 -0500

    testing

---
+++ tests/resta.test.js
@@ -0,0 +1,5 @@
+const resta = require('../resta.js');
+
+test('resta 5 - 3 es igual a 2 ', () => {
+  expect (resta(5,3)) .toBe(2);
+})
---
+++ tests/string.test.js
@@ -0,0 +1,5 @@
+const string = require('../string.js');
+
+test('debera contener una a', () => {
+  expect (string("hola")).toBe(true);
+})
